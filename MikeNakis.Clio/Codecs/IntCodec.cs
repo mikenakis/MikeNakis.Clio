8a41b7b9cba1d978639ddb71ff344bb40c76cb98
MikeNakis <git@michael.gr>
2024-12-09 17:08:25 +0100

    initial commit.

---
+++ MikeNakis.Clio/Codecs/IntCodec.cs
@@ -0,0 +1,26 @@
+namespace MikeNakis.Clio.Codecs;
+
+using Sys = System;
+using SysGlob = System.Globalization;
+
+public sealed class IntCodec : StructCodec<int>
+{
+	public static readonly StructCodec<int> Instance = new IntCodec();
+
+	IntCodec()
+	{ }
+
+	public override string Name => "integer";
+
+	public override int ValueFromText( string text )
+	{
+		if( !int.TryParse( text, SysGlob.CultureInfo.InvariantCulture, out int value ) )
+			throw new Sys.FormatException( $"Expected an integer, found '{text}'" );
+		return value;
+	}
+
+	public override string TextFromValue( int value )
+	{
+		return value.ToString( SysGlob.CultureInfo.InvariantCulture );
+	}
+}
