309330533f2cf1e97163f77e20c5e9ba50a24359
MikeNakis <git@michael.gr>
2025-10-31 12:35:21 +0100

    introduced DevWebServer

--- MikeNakis.Clio/Extensions/ArgumentParserExtensions.cs
+++ MikeNakis.Clio/Extensions/ArgumentParserExtensions.cs
@@ -63,6 +63,19 @@ public static class ArgumentParserExtensions
 		return self.AddOption( name, IntCodec.Instance, singleLetterName, description, parameterName, presetValue );
 	}
 
+	///<summary>Adds an option of type <c>int</c> with a default value.</summary>
+	///<param name="self">The <see cref="ArgumentParser" />.</param>
+	///<param name="name">The name of the option.</param>
+	///<param name="singleLetterName">The (optional) single-letter name for the option.</param>
+	///<param name="description">The description of the option, for use when displaying help.</param>
+	///<param name="parameterName">The name of the parameter of the option, for use when displaying help.</param>
+	///<param name="defaultValue">The default value, which will be the value of the option if the option is omitted.</param>
+	public static IOptionArgument<int> AddIntOptionWithDefault( this BaseArgumentParser self, string name, int defaultValue, char? singleLetterName = null, //
+		string? description = null, string? parameterName = null )
+	{
+		return self.AddOptionWithDefault( name, IntCodec.Instance, defaultValue, singleLetterName, description, parameterName );
+	}
+
 	///<summary>Adds a positional argument of type <c>string</c>.</summary>
 	///<param name="self">The <see cref="ArgumentParser" />.</param>
 	///<param name="name">The name of the positional argument, for use in response files, and when displaying help.</param>
